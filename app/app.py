d249a0917880b3bd79f96c24a743699355bb031e
OrionChocoPie <nogayev.nuras@gmail.com>
2021-05-20 00:43:20 +0300

    refactor code

--- app/app.py
+++ app/app.py
@@ -14,10 +14,10 @@ import plotly.graph_objects as go
 
 from datetime import datetime as dt
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 # цвета для графиков
-blue = '#7bc7ff'
+blue = "#7bc7ff"
 green = "#92d8d8"
 red = "#fac1b7"
 
@@ -29,43 +29,42 @@ app = dash.Dash(
 server = app.server
 app.config["suppress_callback_exceptions"] = True
 
-true_data = pd.read_csv('data/original.csv')
-regions = true_data['region'].unique()
+true_data = pd.read_csv("data/original.csv")
+regions = true_data["region"].unique()
 
-predicted_data = pd.read_csv('data/submit.csv')
-id_info = predicted_data['id'].str.split('_', expand=True)
-id_info.columns = ['region', 'date', 'hour', 'pred_number']
-predicted_data = pd.concat((id_info, predicted_data[['y']]), axis=1)
+predicted_data = pd.read_csv("data/submit.csv")
+id_info = predicted_data["id"].str.split("_", expand=True)
+id_info.columns = ["region", "date", "hour", "pred_number"]
+predicted_data = pd.concat((id_info, predicted_data[["y"]]), axis=1)
 
-data = true_data[['region', 'tpep_pickup_datetime', 'T+1']]
-data.columns = ['region', 'datetime', 'y']
-data['datetime'] = pd.to_datetime(data['datetime'])
-data['y'] = data['y'].astype(float)
+data = true_data[["region", "tpep_pickup_datetime", "T+1"]]
+data.columns = ["region", "datetime", "y"]
+data["datetime"] = pd.to_datetime(data["datetime"])
+data["y"] = data["y"].astype(float)
 
-pred_data = predicted_data[predicted_data['pred_number'] == '1'].drop('pred_number', axis=1)
-pred_data['datetime'] = pred_data['date'] + ' ' + pred_data['hour'] + ':00:00'
-pred_data['datetime'] = pd.to_datetime(pred_data['datetime'])
-pred_data['y'] = pred_data['y'].astype(float)
-pred_data.drop(['date', 'hour'], axis=1, inplace=True)
+pred_data = predicted_data[predicted_data["pred_number"] == "1"].drop(
+    "pred_number", axis=1
+)
+pred_data["datetime"] = pred_data["date"] + " " + pred_data["hour"] + ":00:00"
+pred_data["datetime"] = pd.to_datetime(pred_data["datetime"])
+pred_data["y"] = pred_data["y"].astype(float)
+pred_data.drop(["date", "hour"], axis=1, inplace=True)
 
-coordinates = pd.read_csv('data/regions.csv', sep=';')
-coordinates = coordinates[coordinates['region'].isin(regions)]
-coordinates.set_index('region', inplace=True)
-geo = {
-    'type': 'FeatureCollection',
-    'features': []
-}
+coordinates = pd.read_csv("data/regions.csv", sep=";")
+coordinates = coordinates[coordinates["region"].isin(regions)]
+coordinates.set_index("region", inplace=True)
+geo = {"type": "FeatureCollection", "features": []}
 for region, row in coordinates.iterrows():
     cur_coords = row.values
-    geo['features'].append(
+    geo["features"].append(
         {
-            'type': 'Feature',
-            'properties': {
-                'REGION': region,
+            "type": "Feature",
+            "properties": {
+                "REGION": region,
             },
-            'geometry': {
-                'type': 'Polygon',
-                'coordinates': [
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
                     [
                         [cur_coords[0], cur_coords[3]],
                         [cur_coords[1], cur_coords[3]],
@@ -73,9 +72,9 @@ for region, row in coordinates.iterrows():
                         [cur_coords[0], cur_coords[2]],
                         [cur_coords[0], cur_coords[3]],
                     ]
-                ]
+                ],
             },
-            'id': region
+            "id": region,
         },
     )
 
@@ -86,12 +85,8 @@ def build_banner():
             html.H3("", className="one-third column"),
             html.Div(
                 [
-                    html.H3(
-                        "Прогноз вызовов в такси", style={'text-align': 'center'}
-                    ),
-                    html.H5(
-                        "Нью-Йорк", style={'text-align': 'center'}
-                    ),
+                    html.H3("Прогноз вызовов в такси", style={"text-align": "center"}),
+                    html.H5("Нью-Йорк", style={"text-align": "center"}),
                 ],
                 className="one-half column",
             ),
@@ -99,11 +94,12 @@ def build_banner():
                 html.Button("github"),
                 href="https://github.com/OrionChocoPie/Coursera-Taxi-Project",
                 className="one-third column",
-                style={'float': 'right'}
-            )
+                style={"float": "right"},
+            ),
         ],
         className="row flex-display",
     )
+
 
 @app.callback(
     Output("time-series-plot", "figure"),
@@ -113,33 +109,36 @@ def build_banner():
 )
 def render_time_series(region):
     fig = go.Figure()
-    cur_true_data = data[data['region'] == region]
-    cur_pred_data = pred_data[pred_data['region'] == str(region)]
+    cur_true_data = data[data["region"] == region]
+    cur_pred_data = pred_data[pred_data["region"] == str(region)]
 
     fig.add_trace(
         go.Scatter(
-            x=cur_true_data['datetime'], y=cur_true_data['y'],
-            name='Реальное',
+            x=cur_true_data["datetime"],
+            y=cur_true_data["y"],
+            name="Реальное",
             line=dict(shape="spline", smoothing=1.05, width=1, color=green),
-            marker=dict(symbol='circle-open'),
+            marker=dict(symbol="circle-open"),
         )
     )
     fig.add_trace(
         go.Scatter(
-            x=cur_pred_data['datetime'], y=cur_pred_data['y'],
-            name='Прогноз',
+            x=cur_pred_data["datetime"],
+            y=cur_pred_data["y"],
+            name="Прогноз",
             line=dict(shape="spline", smoothing=1.05, width=1, color=red),
-            marker=dict(symbol='circle-open'),
+            marker=dict(symbol="circle-open"),
         )
     )
 
     fig.update_layout(
         plot_bgcolor="#F9F9F9",
         paper_bgcolor="#F9F9F9",
-        margin={"r":0,"t":0,"l":0,"b":0}
+        margin={"r": 0, "t": 0, "l": 0, "b": 0},
     )
 
     return fig
+
 
 @app.callback(
     Output("map", "figure"),
@@ -148,28 +147,32 @@ def render_time_series(region):
     ],
 )
 def render_map(date):
-    cur_pred_data = pred_data[pred_data['datetime'] == pd.to_datetime(date)]
+    cur_pred_data = pred_data[pred_data["datetime"] == pd.to_datetime(date)]
 
-    fig = go.Figure(go.Choroplethmapbox(
+    fig = go.Figure(
+        go.Choroplethmapbox(
             geojson=geo,
-            locations=cur_pred_data['region'],
-            z=cur_pred_data['y'],
-            colorscale=[[0, 'rgb(184, 225, 255)'], [1, 'rgb(0, 147, 255)']],
-            showscale = False,
-            text='region: ' + cur_pred_data['region'] + ' taxi: ' + cur_pred_data['y'].astype(int).astype(str),
-            hoverinfo='text',
+            locations=cur_pred_data["region"],
+            z=cur_pred_data["y"],
+            colorscale=[[0, "rgb(184, 225, 255)"], [1, "rgb(0, 147, 255)"]],
+            showscale=False,
+            text="region: "
+            + cur_pred_data["region"]
+            + " taxi: "
+            + cur_pred_data["y"].astype(int).astype(str),
+            hoverinfo="text",
             marker_opacity=0.7,
         )
     )
 
     fig.update_layout(
-        mapbox = {
-            'style': "carto-positron",
-            'center': {'lon': -73.98519, 'lat': 40.74843},
-            'zoom': 10
+        mapbox={
+            "style": "carto-positron",
+            "center": {"lon": -73.98519, "lat": 40.74843},
+            "zoom": 10,
         },
-        margin={"r":0,"t":0,"l":0,"b":0},
-        showlegend = False
+        margin={"r": 0, "t": 0, "l": 0, "b": 0},
+        showlegend=False,
     )
 
     return fig
@@ -186,12 +189,10 @@ app.layout = html.Div(
                 html.Div(
                     className="mini_container fourth columns",
                     children=[
-                        html.Label(children='Выберите область'),
+                        html.Label(children="Выберите область"),
                         dcc.Dropdown(
                             id="region-selector",
-                            options=[
-                                {'label': i, 'value': i} for i in regions
-                            ],
+                            options=[{"label": i, "value": i} for i in regions],
                             value=regions[0],
                             searchable=False,
                         ),
@@ -200,13 +201,13 @@ app.layout = html.Div(
                 html.Div(
                     className="mini_container second columns",
                     children=[
-                        html.Label(children='Выберите дату'),
+                        html.Label(children="Выберите дату"),
                         dcc.DatePickerSingle(
-                            id='date-picker',
+                            id="date-picker",
                             min_date_allowed=dt(2016, 6, 1),
                             max_date_allowed=dt(2016, 6, 30),
                             initial_visible_month=dt(2016, 6, 1),
-                            date=str(dt(2016, 6, 1))
+                            date=str(dt(2016, 6, 1)),
                         ),
                     ],
                 ),
@@ -215,11 +216,11 @@ app.layout = html.Div(
         ),
         html.Div(
             className="pretty_container",
-            children=[dcc.Graph(id='time-series-plot')],
+            children=[dcc.Graph(id="time-series-plot")],
         ),
         html.Div(
             className="pretty_container",
-            children=[dcc.Graph(id='map')],
+            children=[dcc.Graph(id="map")],
         ),
     ],
 )
--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -3,62 +3,66 @@ import pandas as pd
 
 from scipy.stats import binned_statistic_2d
 
-regions = pd.read_csv('regions.csv', sep=';')
-longitude_bins = (regions['west'].append(regions['east'])).unique()
-latitude_bins = (regions['south'].append(regions['north'])).unique()
+regions = pd.read_csv("regions.csv", sep=";")
+longitude_bins = (regions["west"].append(regions["east"])).unique()
+latitude_bins = (regions["south"].append(regions["north"])).unique()
+
 
 def preprocessing(date):
-    df = pd.read_csv('yellow_tripdata_' + date + '.csv') # '2016-05'
+    df = pd.read_csv("yellow_tripdata_" + date + ".csv")  # '2016-05'
 
     # Перевод строки во временные данные
-    df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
-    df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
-    df['trip_distance'] = df['trip_distance'].astype(float)
+    df["tpep_dropoff_datetime"] = pd.to_datetime(df["tpep_dropoff_datetime"])
+    df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
+    df["trip_distance"] = df["trip_distance"].astype(float)
 
     # Фильтрация
-    df = df[(df['tpep_dropoff_datetime'] - df['tpep_pickup_datetime']).dt.seconds > 0]
-    df = df[df['passenger_count'] > 0]
-    df = df[df['trip_distance'] > 0]
+    df = df[(df["tpep_dropoff_datetime"] - df["tpep_pickup_datetime"]).dt.seconds > 0]
+    df = df[df["passenger_count"] > 0]
+    df = df[df["trip_distance"] > 0]
     df = df[
-        (-74.25559 <= df['pickup_longitude']) &
-        (df['pickup_longitude'] <= -73.70001) &
-        (40.49612 <= df['pickup_latitude']) &
-        (df['pickup_latitude'] <= 40.91553)
+        (-74.25559 <= df["pickup_longitude"])
+        & (df["pickup_longitude"] <= -73.70001)
+        & (40.49612 <= df["pickup_latitude"])
+        & (df["pickup_latitude"] <= 40.91553)
     ]
 
     # Посчитаем номера регионов
     new_regions = binned_statistic_2d(
-        df['pickup_longitude'],
-        df['pickup_latitude'],
-        df['pickup_longitude'],
+        df["pickup_longitude"],
+        df["pickup_latitude"],
+        df["pickup_longitude"],
         bins=[longitude_bins, latitude_bins],
-        statistic='count',
-        expand_binnumbers=True
+        statistic="count",
+        expand_binnumbers=True,
     )
 
     # Полученные номера регионов присвоим поездкам
-    df['region'] = (new_regions.binnumber[0] - 1) * 50 + new_regions.binnumber[1]
+    df["region"] = (new_regions.binnumber[0] - 1) * 50 + new_regions.binnumber[1]
 
     # Удаляем минуты и секунды из времени начала поездки
-    df['tpep_pickup_datetime'] = df['tpep_pickup_datetime'].dt.floor('H')
+    df["tpep_pickup_datetime"] = df["tpep_pickup_datetime"].dt.floor("H")
 
     # Агрегируем данные по часам и регионам
-    time_region = pd.crosstab(df['tpep_pickup_datetime'], df['region'])
+    time_region = pd.crosstab(df["tpep_pickup_datetime"], df["region"])
     # заполняем нулями регионы, в которых нету данных
     for i in range(1, 2501):
         if i not in time_region.columns:
             time_region[i] = 0
     time_region = time_region[sorted(time_region.columns)]
-    
-    time_region.to_csv('time_region_' + date +'.csv')
+
+    time_region.to_csv("time_region_" + date + ".csv")
+
 
 from os import listdir
 from os.path import isfile, join
 import re
 
-files = [f for f in listdir('.') if isfile(join('.', f)) and f.startswith('yellow_tripdata')]
+files = [
+    f for f in listdir(".") if isfile(join(".", f)) and f.startswith("yellow_tripdata")
+]
 
-preprocessing('2016-07')
+preprocessing("2016-07")
 # for file in files:
 #     date = re.split('_|\.', file)[-2]
 #     preprocessing(date)
